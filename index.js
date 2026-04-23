cae46ebf237f379dc058880b4303f0b781c10c8a
ChanchayMateo <a00840391@tec.mx>
2026-04-23 14:36:30 -0600

    cambios

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import "dotenv/config"
 import express from "express"
-import morgan  from "morgan"
+import morgan from "morgan"
+import cors from "cors"
 import indexRoutes from "./routes/index.routes.js"
 import loginRoutes from "./routes/login.routes.js"
 import usersRoutes from "./routes/users.routes.js"
 import { connectDb } from "./utils/db.js"
 
+const app = express() // ✅ PRIMERO creas app
+
 connectDb()
 
-const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(indexRoutes)
-app.use(loginRoutes)
-app.use(usersRoutes)
 
+
+app.use("/api", indexRoutes)
+app.use("/api", loginRoutes)
+app.use("/api", usersRoutes)
 
 const PORT = 8000
-app.listen(PORT,console.log("http://localhost:"+PORT))
+app.listen(PORT, () => {
+  console.log("http://localhost:" + PORT)
+})
