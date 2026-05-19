3eb7c1fadd099eb3032aefa9a3578966c7ad7f17
ChanchayMateo <a00840391@tec.mx>
2026-05-19 09:13:32 -0600

    jwt

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import {hash} from "../utils/hash.js"
+import jwt from "jsonwebtoken"
 
 export const login = async (req, res) => {
     const {username, password} = req.body
@@ -7,9 +8,10 @@ export const login = async (req, res) => {
     const salt = user.password.substring(0, process.env.SALT_SIZE)
     const hased = hash(password, salt)
     if (user.password === hased){
-        res.json({login:true, msg:"ok", user:user})
+        const token = jwt.sign({id:user._id}, process.env.JWT, {expiresIn:"1h"})
+        res.json({login:true, msg:"ok", user:user, token:token})
     }
     else {
-        res.status(404).json({login:false, msg:"no ok", user:{}})
+        res.status(404).json({login:false, msg:"no ok", user:{}, token:""})
     }
 }
